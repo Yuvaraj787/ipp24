ba7d5b0432d9cd72ed9eef46dfaec8cd31f2464c
Sri Dharani R <sridharanirajendran26@gmail.com>
2024-10-22 00:00:26 +0530

    navigationdropdown

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
         <div className="mx-4 flex justify-center text-6xl text-indigo-700 items-center">
           <p className='xl:-rotate-90'>ISTA</p>
         </div>
-        <div className="text-center text-2xl text-gray-500 mx-4">
+        <div className="text-center text-2xl text-gray-400 mx-4">
           <p className="leading-9 mx-4">
             Department of Information Science and Technology is one of the youngest and fastest growing departments of Anna University. Under this department, we, ISTA, are a staff-student run body striving to work hard for the betterment of our department, host various technical symposiums and represent IST in various activities. We also aim at guiding students across all years with a variety of matters, for a better tomorrow.
           </p>
@@ -18,14 +18,14 @@ export default function About() {
       </div>
   
       {/* Vertical Line Separator */}
-      <div className="border-l-2 border-gray-500 h-96 mx-4 hidden xl:block"></div>
-      <div className="border-t-2 mt-6 border-gray-500 h-2 xl:hidden"></div>
+      <div className="border-l-2 border-gray-400 h-98 mx-4 hidden xl:block"></div>
+      <div className="border-t-2 mt-6 border-gray-400 h-2 xl:hidden"></div>
       {/* I++ Section */}
       <div className="flex-1 flex flex-col xl:flex-row">
       <div className="mx-4 flex justify-center items-center text-6xl text-indigo-700">
           <p className='xl:hidden '>I++</p>
         </div>
-        <div className="text-center mx-4 text-2xl text-gray-500">
+        <div className="text-center mx-4 text-2xl text-gray-400">
           <p className=" leading-9 mx-4">
             Held every year in the odd semester, I++ is the intra-department symposium held by ISTA. With a footfall of about 800 odd students every year, we strive hard to do our part in bringing together the student community of not just our department, but the entire community at the College of Engineering, Guindy.
           </p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,8 +50,10 @@ export default function Header() {
   };
 
   return (
-    <header className="sticky top-0 z-50 bg-gradient-to-r from-[#151b23] via-blue-950 to-[#151b23] flex justify-between items-center">
-      
+    <>
+    
+      {isLg ? (
+      <header className="sticky top-0 z-50 bg-gradient-to-r from-[#151b23] via-blue-950 to-[#151b23] flex justify-between items-center">
       <div className="basis-1/3">
         <img src={logoLeft} alt="Left Image" className="mx-4 w-12 h-12 md:w-13 md:h-13" />
       </div>
@@ -61,29 +63,43 @@ export default function Header() {
       </div>
 
       <nav className="basis-1/3 relative flex justify-end">
-        {isLg ? (
-          <ul className="sm:visible flex text-white text-1xl gap-8 px-4 flex-row font-semibold">
-            <li className="p-2 cursor-pointer"><a onClick={() => navigate('/')}>Home</a></li>
-            <li className="p-2 cursor-pointer"><a onClick={handleAboutClick}>About</a></li>
-            <li className="p-2 cursor-pointer"><a onClick={handleEventsClick}>Events</a></li>
-            <li className="p-2 cursor-pointer"><a href="#contact">Contact</a></li>
+        
+          <ul className="flex text-white text-1xl gap-8 px-4 flex-row font-semibold">
+            <li className="p-2 hover:text-slate-200 cursor-pointer" onClick={() => navigate('/')}>Home</li>
+            <li className="p-2 hover:text-slate-200 cursor-pointer" onClick={handleAboutClick}>About</li>
+            <li className="p-2 hover:text-slate-200 cursor-pointer" onClick={handleEventsClick}>Events</li>
+            <li className="p-2 hover:text-slate-200 cursor-pointer"><a href="#contact">Contact</a></li>
           </ul>
+        </nav>
+      </header>
         ) : (
-          <>
-            <button onClick={toggleDropdown} className="text-4xl px-3 mt-2">
-              ☰
-            </button>
+          <header className="sticky top-0 z-50 bg-gradient-to-r from-[#151b23] via-blue-950 to-[#151b23] flex justify-between items-center">
+
+            <div className="basis-1/3">
+            <img src={logoLeft} alt="Left Image" className="mx-4 w-12 h-12 md:w-13 md:h-13" />
+            </div>
+
+            <div className="basis-1/3">
+              <img src={logoCenter} alt="Center Image" className="w-28 h-24 md:w-23 md:h-26 mx-auto" />
+            </div>
+
+            <nav className="basis-1/3 relative flex justify-end">
+              <button onClick={toggleDropdown} className="text-4xl px-3 mt-2 text-white">
+                ☰
+              </button>
+            </nav>
             {isDropdownOpen && (
-              <ul className="absolute right-0 mt-20 bg-white text-black shadow-lg rounded-lg z-10">
-                <li className="p-2 hover:bg-gray-200 cursor-pointer"><a onClick={() => navigate('/')}>Home</a></li>
-                <li className="p-2 hover:bg-gray-200 cursor-pointer"><a onClick={handleAboutClick}>About</a></li>
-                <li className="p-2 hover:bg-gray-200 cursor-pointer"><a onClick={handleEventsClick}>Events</a></li>
-                <li className="p-2 hover:bg-gray-200 cursor-pointer"><a href="#contact">Contact</a></li>
+              <ul className="absolute right-0 top-full  bg text-center flex flex-col  justify-center bg-gradient-to-r from-[#151b23] via-blue-950 to-[#151b23] w-full items-center">
+                <li className="p-2 hover:text-slate-200 cursor-pointer" onClick={() => { setDropdownOpen(false); navigate('/'); }}>Home</li>
+                <li className="p-2  hover:text-slate-200 cursor-pointer" onClick={() => { setDropdownOpen(false); handleAboutClick(); }}>About</li>
+                <li className="p-2  hover:text-slate-200 cursor-pointer" onClick={() => { setDropdownOpen(false); handleEventsClick(); }}>Events</li>
+                <li className="p-2  hover:text-slate-200 cursor-pointer"><a href="#contact" onClick={() => setDropdownOpen(false)}>Contact</a></li>
               </ul>
             )}
-          </>
+          
+            
+          </header>
         )}
-      </nav>
-    </header>
+      </>
   );
 }
